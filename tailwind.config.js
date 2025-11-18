d901b7adf46e2c1f0ab4398ba79ee6e621177fc5
MasalemaCristian <masalemac@gmail.com>
2025-11-17 23:14:08 -0500

    fix: añadir safelist en Tailwind para clases dinámicas (botón 'Ir a la oferta' verde)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
     "./public/**/*.js",
     "./src/**/*.css"
   ],
+  safelist: [
+    // Clases dinámicas generadas por JS
+    'bg-green-600',
+    'text-white',
+    'px-3',
+    'py-1',
+    'rounded',
+    'text-sm',
+    'text-center',
+    'opacity-50',
+    'pointer-events-none',
+    'flex',
+    'gap-2',
+    'mt-3'
+  ],
   theme: {
     extend: {}
   },
